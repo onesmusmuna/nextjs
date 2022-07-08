110d6d74d090f37fbe20f9180389c9e58b26881b
onesmusmuna <onesmusmuna@gmail.com>
2022-07-08 21:25:45 +0300

    Link component

--- next-routing/pages/index.js
+++ next-routing/pages/index.js
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <h1>Home route</h1>
+
+      <Link href="/blog">
+        <a>Blog</a>
+      </Link>
+      <Link href="/product">
+        <a>product</a>
+      </Link>
     </>
   );
 }
--- next-routing/pages/product/index.js
+++ next-routing/pages/product/index.js
@@ -1,7 +1,27 @@
+import Link from "next/link";
+
 export default function Product() {
   return (
     <>
-      <h1>Product route</h1>
+      <h1>Product List</h1>
+
+      <h2>
+        <Link href="/product/1">
+          <a>Product 1</a>
+        </Link>
+      </h2>
+
+      <h2>
+        <Link href="/product/2">
+          <a>Product 2</a>
+        </Link>
+      </h2>
+
+      <h2>
+        <Link href="/product/3" replace>
+          <a>Product 3</a>
+        </Link>
+      </h2>
     </>
   );
 }
